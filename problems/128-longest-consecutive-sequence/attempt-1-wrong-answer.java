b4b8c0a59940ded875c70cf020197bc90f76a565
Vidhi Jindal <vidhijindal1686@gmail.com>
2026-09-04 12:47:15 +0530

    Wrong Answer: Longest Consecutive Sequence (attempt 1)

---
+++ problems/128-longest-consecutive-sequence/attempt-1-wrong-answer.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        HashSet <Integer> set = new HashSet<>();
+        for(int num:nums){
+            set.add(num);
+        }
+        int max=1;
+        for(int num :set){
+            if(!set.contains(num-1)){
+                int current = num;
+                int count =1;
+                  while(set.contains(current+1)){
+                current++;
+                count++;
+            }
+            max= Math.max(max, count);
+            }
+          
+            
+        }
+        return max;
+        
+    }
+}
