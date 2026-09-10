d14320a0e6aed7dabc9afb27df5046ac0f9051d8
Vidhi Jindal <vidhijindal1686@gmail.com>
2026-09-10 11:24:02 +0530

    Accepted: Daily Temperatures (attempt 2)

---
+++ problems/739-daily-temperatures/attempt-2-accepted.java
@@ -0,0 +1,18 @@
+
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>(); // store indexes
+
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && temperatures[i] > temperatures[st.peek()]) {
+                int idx = st.pop();
+                ans[idx] = i - idx;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+}
