76fafe13e0d9af1549c791488bcc0069e859ae61
Vidhi Jindal <vidhijindal1686@gmail.com>
2026-09-10 11:13:50 +0530

    Accepted: Calculate Special Bonus (attempt 1)

---
+++ problems/1873-calculate-special-bonus/attempt-1-accepted.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+-- SOLUTION  - 1 :
+        -- select entries with odd  employee id and name not start with M
+            select employee_id , salary as bonus 
+            from employees 
+            where employee_id%2 <>0 and name not like 'M%'
+            
+        -- join both selection 
+            union
+            
+        -- select remaining entries from table 
+            select employee_id , 0 as bonus
+            from employees
+            where employee_id%2 = 0 or name like 'M%'
+            order by employee_id;
+            
