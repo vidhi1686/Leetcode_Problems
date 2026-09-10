b9e37852fcaed7a44b43fae9ee32e966a4ee39a1
Vidhi Jindal <vidhijindal1686@gmail.com>
2026-09-10 11:10:49 +0530

    Accepted: Bank Account Summary II (attempt 1)

---
+++ problems/1587-bank-account-summary-ii/attempt-1-accepted.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT
+    name,
+    SUM(amount) AS balance
+FROM Users u
+INNER JOIN Transactions t
+ON u.account = t.account
+GROUP BY name
+HAVING SUM(amount) > 10000;
