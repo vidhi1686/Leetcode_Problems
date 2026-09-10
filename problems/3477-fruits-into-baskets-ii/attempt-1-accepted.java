33eb270ea546cd8eb441ce8bec051984e08c0aa8
Vidhi Jindal <vidhijindal1686@gmail.com>
2026-09-10 11:05:36 +0530

    Accepted: Fruits Into Baskets II (attempt 1)

---
+++ problems/3477-fruits-into-baskets-ii/attempt-1-accepted.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int count=0;
+
+        for(int i=0;i<fruits.length;i++){
+            for(int j=0;j<baskets.length;j++){
+                if(fruits[i]<=baskets[j]){
+                    count++;
+                    baskets[j]=-1;
+                    break;
+                }
+
+            }
+        }
+        return fruits.length - count;
+        
+    }
+}
