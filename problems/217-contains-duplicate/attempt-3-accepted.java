8710f67a01cff9fe62e6b826d78c7247e8e8db09
Vidhi Jindal <vidhijindal1686@gmail.com>
2026-09-10 11:48:51 +0530

    Accepted: Contains Duplicate (attempt 3)

---
+++ problems/217-contains-duplicate/attempt-3-accepted.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+// TIME LIMIT WILL EXCEED
+        
+        // for(int i=0 ; i<nums.length ;i++){
+        //     for (int j=i+1 ; j<nums.length ;j++){
+        //         if(nums[i]==nums[j]){
+        //             return true;
+        //         }
+
+        //     }
+        // }
+        // return false;
+
+        // Arrays.sort(nums);
+        // for(int i =0; i<nums.length-1; i++){
+        //     if(nums[i]== nums[i+1]){
+        //         return true;
+        //     }
+        // }
+        // return false;
+        
+        // BY HASHSET 
+        HashSet <Integer> set = new HashSet <>();
+        for(int i=0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            else{
+                set.add(nums[i]);
+            }
+        }
+        return false;
+       
+    }
+   
+}
