6abe8c311c78b2617c4e7dc6007213d5d894bbe2
Vidhi Jindal <vidhijindal1686@gmail.com>
2026-09-10 11:45:18 +0530

    Accepted: Majority Element (attempt 7)

---
+++ problems/169-majority-element/attempt-7-accepted.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+    //     int n = nums.length;
+
+    //     for (int i = 0; i < n; i++) {
+    //         int count = 0;
+
+    //         // Count how many times nums[i] appears
+    //         for (int j = 0; j < n; j++) {
+    //             if (nums[j] == nums[i]) {
+    //                 count++;
+    //             }
+    //         }
+
+    //         // If it appears more than half → return it
+    //         if (count > n / 2) {
+    //             return nums[i];
+    //         }
+    //     }
+
+    //     return -1; // Not needed if majority always exists
+
+       
+      
+        // for(int num:nums){
+        //     int count=0;
+        //     for(int x:nums){
+        //         if(x==num){
+        //             count++;
+        //         }
+        //     }
+        //     if(count>nums.length/2){
+        //         return num;
+        //     }
+        // }
+        // return -1;
+
+
+        // ANOTHER APPROACH
+
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
+}
