708bc6c795887bf51f02be97f50413f4171c8900
Vidhi Jindal <vidhijindal1686@gmail.com>
2026-09-10 11:24:27 +0530

    Accepted: Number of Recent Calls (attempt 1)

---
+++ problems/933-number-of-recent-calls/attempt-1-accepted.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class RecentCounter {
+    Queue<Integer> q = new LinkedList<>();
+    public int ping(int t) {
+        q.add(t);
+        while (q.peek() < t - 3000)
+            q.poll();
+        return q.size();
+    }
+}
