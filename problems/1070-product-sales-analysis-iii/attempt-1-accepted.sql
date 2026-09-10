069a589712641c91790c6f95b2798153044db8b0
Vidhi Jindal <vidhijindal1686@gmail.com>
2026-09-10 11:15:55 +0530

    Accepted: Product Sales Analysis III (attempt 1)

---
+++ problems/1070-product-sales-analysis-iii/attempt-1-accepted.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+select product_id ,year as first_year , quantity , price 
+from Sales
+where (product_id , year) in(
+    select product_id 
+    ,min(year) as first_y
+    from Sales
+    Group BY product_id)
