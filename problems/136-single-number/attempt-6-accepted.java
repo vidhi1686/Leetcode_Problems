14a71adbce908654706d12b44fbe3b0473270434
Vidhi Jindal <vidhijindal1686@gmail.com>
2026-09-10 10:59:12 +0530

    Accepted: Single Number (attempt 6)

---
+++ problems/136-single-number/attempt-6-accepted.java
@@ -0,0 +1,19 @@
+class Solution 
+{
+    public int singleNumber(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j]&&i!=j ) {
+                    count++;
+                }
+            }
+
+            if (count == 0) {
+                return nums[i];
+            }
+        }
+        return -1; 
+    }
+}
