e155cd2ffb415acf829635402e602834ca75c9f0
Vidhi Jindal <vidhijindal1686@gmail.com>
2026-09-10 11:37:17 +0530

    Accepted: Implement Queue using Stacks (attempt 3)

---
+++ problems/232-implement-queue-using-stacks/attempt-3-accepted.java
@@ -0,0 +1,36 @@
+class MyQueue {
+
+    private Stack<Integer> input;
+    private Stack<Integer> output;
+
+    public MyQueue() {
+        input = new Stack<>();
+        output = new Stack<>();
+    }
+    
+    // Push element x to the back of queue
+    public void push(int x) {
+        input.push(x);
+    }
+    
+    // Removes the element from in front of queue and returns that element
+    public int pop() {
+        peek(); // Make sure output has current front
+        return output.pop();
+    }
+    
+    // Get the front element
+    public int peek() {
+        if (output.isEmpty()) {
+            while (!input.isEmpty()) {
+                output.push(input.pop());
+            }
+        }
+        return output.peek();
+    }
+    
+    // Returns whether the queue is empty
+    public boolean empty() {
+        return input.isEmpty() && output.isEmpty();
+    }
+}
