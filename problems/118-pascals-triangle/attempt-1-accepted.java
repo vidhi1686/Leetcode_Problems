291b88c4170591da3b59863eeae77938d81bd4f7
Vidhi Jindal <vidhijindal1686@gmail.com>
2026-09-10 11:42:12 +0530

    Accepted: Pascal's Triangle (attempt 1)

---
+++ problems/118-pascals-triangle/attempt-1-accepted.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        ArrayList<List<Integer>> result =new ArrayList <> ();
+        for (int i=0; i<numRows ;i++){
+            ArrayList <Integer> currentRow =new ArrayList <> ();
+            for(int pos=0 ; pos <= i ; pos++){
+                if(pos==0 || pos==i){
+                    currentRow.add(pos,1);
+                }
+                else{
+                    List<Integer> prevRow = result.get(i - 1);
+                    int sum = prevRow.get(pos-1) + prevRow.get(pos);
+                    currentRow.add(pos,sum);
+                }    
+            }
+            result.add(currentRow);
+
+        }
+        return result;
+        
+    }
+}
